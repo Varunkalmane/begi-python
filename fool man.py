0e8147442acdb7ed160c1045fbe9267311973b0c
Varunkalmane <varunkgkalmane2006@gmail.com>
2024-09-05 15:05:27 +0530

    Add files via upload
    just an BEGINNING

---
+++ fool man.py
@@ -0,0 +1,13 @@
+
+print("Welcome I didnt realise coding is this diffcult until i made a man using stars")
+print("To see what i made please enter your name")
+name=input("NAME = ")
+print("hey ",name)
+print("  @\n  *\n * * \n* *  *\n  *\n  *\n*   *")
+print("To see its code please press password VARUN")
+varun=input("Enter your password :")
+if(varun=="VARUN"):
+    print("LESSON NUMBER 1 == NEVER EXPECT ANYTHING FREE")
+else:
+    print("WRONG password,please try again")
+input() 
